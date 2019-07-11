2b450c6ac0a7305e5a71831d363b221ab0a7073b
Grzegorz Bach <g.bach90@gmail.com>
2019-07-11 21:01:31 +0200

    test dmd feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import initState from './initState';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
 function App() {
+  const [state, setState] = useState(initState);
+
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+  };
+
+  const onDragEnd = result => {
+    if (result.destination === null) {
+      console.log('błąd!')
+    } else if (result.source.droppableId === result.destination.droppableId) {
+      setState({
+        ...state,
+        columns: {
+          ...state.columns,
+          [result.source.droppableId]: {
+            ...state.columns[result.source.droppableId],
+            items: reorder(state.columns[result.source.droppableId].items, result.source.index, result.destination.index)
+          }
+        }
+      })
+    } else {
+      let oldColumn = Array.from(state.columns[result.source.droppableId].items);
+      let newColumn = Array.from(state.columns[result.destination.droppableId].items);
+      const [removed] = oldColumn.splice(result.source.index, 1);
+      newColumn = [...newColumn.slice(0, result.destination.index), removed, ...newColumn.slice(result.destination.index),];
+      setState({
+        ...state,
+        columns: {
+          ...state.columns,
+          [result.source.droppableId]: {
+            ...state.columns[result.source.droppableId],
+            items: oldColumn
+          },
+          [result.destination.droppableId]: {
+            ...state.columns[result.source.droppableId],
+            items: newColumn
+          }
+        }
+      })
+      console.log(oldColumn);
+      console.log(newColumn);
+    }
+
+
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <div className="container">
+          {state.columnOrder.map(columnId => (
+            <div key={columnId} id={columnId} className="column">
+              <h2>{state.columns[columnId].title}</h2>
+              <Droppable droppableId={columnId} isDropDisabled={columnId === 'column-1'}>
+                {provided => (
+                  <ul className="list"
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                  >
+                    {state.columns[columnId].items.map((itemId, index) => (
+                      <Draggable key={itemId} draggableId={itemId} index={index} style={{ margin: '10px auto' }}>
+                        {(provided, snapshot) => (
+                          <li className="list-item"
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                          >
+                            {state.itemsList[itemId].content}
+                          </li>
+                        )}
+                      </Draggable>
+                    ))}
+                    {columnId !== 'column-1' ? provided.placeholder : ''}
+                  </ul>
+                )}
+              </Droppable>
+            </div>
+          ))}
+        </div>
+      </DragDropContext>
     </div>
   );
 }
---
+++ src/initState.js
@@ -0,0 +1,60 @@
+const initState = {
+    itemsList: {
+        'item-1': {
+            id: 'item-1',
+            content: 'Element 1'
+        },
+        'item-2': {
+            id: 'item-2',
+            content: 'Element 2'
+        },
+        'item-3': {
+            id: 'item-3',
+            content: 'Element 3'
+        },
+        'item-4': {
+            id: 'item-4',
+            content: 'Element 4'
+        },
+        'item-5': {
+            id: 'item-5',
+            content: 'Element 5'
+        },
+        'item-6': {
+            id: 'item-6',
+            content: 'Element 6'
+        },
+        'item-7': {
+            id: 'item-7',
+            content: 'Element 7'
+        },
+        'item-8': {
+            id: 'item-8',
+            content: 'Element 8'
+        },
+        'item-9': {
+            id: 'item-9',
+            content: 'Element 9'
+        }
+    },
+    columns: {
+        'column-1': {
+            id: 'column-1',
+            title: 'Column 1',
+            items: ['item-1', 'item-2', 'item-3']
+        },
+        'column-2': {
+            id: 'column-2',
+            title: 'Column 2',
+            items: ['item-4', 'item-5', 'item-6']
+        },
+        'column-3': {
+            id: 'column-3',
+            title: 'Column 3',
+            items: ['item-7', 'item-8', 'item-9']
+        }
+    },
+    columnOrder: ['column-1', 'column-2', 'column-3']
+}
+
+export default initState;
